e1721d8835c69fd9de76d7faab0024108e5530be
Igor Pelczarski <igorpe144@gmail.com>
2023-12-02 15:53:41 +0100

    test

--- src/SelectField/index.js
+++ src/SelectField/index.js
@@ -1,6 +1,5 @@
 const SelectField = ({setCurrency, currency}) => (
     <select className="form__field" value={currency} onChange={(event) => setCurrency(event.target.value)}>
-        <option>Test</option>
         <option>Euro</option>
         <option>Dolar amerykański</option>
         <option>Funt brytyjski</option>
