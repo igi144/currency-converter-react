f0bac840443e356cc64ba324d6b81cbd2b8dd251
Igor Pelczarski <igorpe144@gmail.com>
2024-01-18 21:40:09 +0100

    required type delated from input

--- src/Form/Input/index.js
+++ src/Form/Input/index.js
@@ -1,7 +1,6 @@
 export const Input = ({setAmount, amount}) => (
     <input
         className="form__field form__field--value"
-        required type="number"
         min="0.1"
         step="any"
         value={amount}
